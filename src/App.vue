1e901065b2b38fbbfe93bda8e85576378eed3629
Minh <minhn88@gmail.com>
2018-03-09 08:35:51 -0500

    Populate todo list. Basic create and complete function.

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,170 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    <div v-if="isLoading" class="progress">
+      <div class="indeterminate"></div>
+    </div>
+    <transition name="expand">
+      <section v-if="showAddTask" class="addTask" @keyup.enter.prevent="newTask">
+          <div class="input-field">
+            <input id="task-name" type="text" class="validate" v-model="newTaskVar.text">
+            <label for="task-name">New Task</label>
+          </div>
+          <div class="input-field">
+            <textarea id="task-notes" class="materialize-textarea" v-model="newTaskVar.notes"></textarea>
+            <label for="task-notes">Notes</label>
+          </div>
+      </section>
+    </transition>
+    <ul class="task-list">
+      <li v-for="task in tasks">
+          <label :for="task.id">
+            <input type="checkbox"  @click="checkTask(task)" class="filled-in" :id="task.id" :checked="task.completed" />
+            <span></span>
+          </label>
+          <div>{{ task.text }}</div>
+      </li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <a class="btn-floating btn-large waves-effect waves-light red" @click="toggleAddForm()"><i class="material-icons">{{ createButton }}</i></a>
   </div>
 </template>
 
 <script>
+var axios = require('axios');// for ajax
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      isLoading: true, //loading page
+      showAddTask: false,
+      createButton: "add",
+      newTaskVar: {type:"todo"},//just todo for now
+      progressWidth: "",
+      tasks: {}
     }
-  }
+  },
+  methods: {
+    checkTask(task){
+      this.isLoading = true;//progress bar
+      if(task.completed == true){ //if true -> uncheck
+        axios.post('https://habitica.com/api/v3/tasks/'+task.id+'/score/down', {'scoreNotes': 'complete the task?'},{
+          'headers': {
+            "x-api-user": "ad0d7cb3-d8c0-4d36-9fd9-5d9d50fc7ca1",
+            "x-api-key": "87b461d3-6ced-4977-b49d-1aacf8067e23"
+            }
+          })
+        .then(
+          response => {
+            task.completed = false;
+            this.isLoading = false;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      }
+      else{// if false -> true
+        axios.post('https://habitica.com/api/v3/tasks/'+task.id+'/score/up', {'scoreNotes': 'complete the task?'},{
+          'headers': {
+            "x-api-user": "ad0d7cb3-d8c0-4d36-9fd9-5d9d50fc7ca1",
+            "x-api-key": "87b461d3-6ced-4977-b49d-1aacf8067e23"
+            }
+          })
+        .then(
+          response => {
+            task.completed = true;
+            this.isLoading = false;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });        
+      }
+      
+    },
+    toggleAddForm(){
+      this.showAddTask = !this.showAddTask;
+      this.createButton = this.createButton == "add" ? "close" : "add";
+    },
+    newTask(){
+      let task = this.newTaskVar;
+      this.isLoading = true;//progress bar
+      console.log(task);
+      axios.post('https://habitica.com/api/v3/tasks/user', task,{
+          'headers': {
+            "x-api-user": "ad0d7cb3-d8c0-4d36-9fd9-5d9d50fc7ca1",
+            "x-api-key": "87b461d3-6ced-4977-b49d-1aacf8067e23"
+            }
+          })
+        .then(
+          response => {
+            this.isLoading = false;
+            this.loadTasks();
+            this.toggleAddForm();
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    loadTasks(){
+      this.isLoading = true;
+      var taskDailys;
+      var taskTodos; 
+      axios.get('https://habitica.com/api/v3/tasks/user?type=dailys', {
+        'headers': {
+          "x-api-user": "ad0d7cb3-d8c0-4d36-9fd9-5d9d50fc7ca1",
+          "x-api-key": "87b461d3-6ced-4977-b49d-1aacf8067e23"
+          }
+        })
+      .then(
+        response => {
+          //this.tasks = response.data.data;
+          taskDailys = JSON.stringify(response.data.data);
+          taskDailys = taskDailys.substr(1).slice(0, -1);
+          return axios.get('https://habitica.com/api/v3/tasks/user?type=todos', {
+        'headers': {
+          "x-api-user": "ad0d7cb3-d8c0-4d36-9fd9-5d9d50fc7ca1",
+          "x-api-key": "87b461d3-6ced-4977-b49d-1aacf8067e23"
+          }
+        });
+      })
+      .then(
+        response => {
+          taskTodos = JSON.stringify(response.data.data);
+          taskTodos = taskTodos.substr(1).slice(0, -1);
+          this.tasks = JSON.parse("[" + taskDailys + "," + taskTodos + "]");
+          this.isLoading = false;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });     
+    }
+  },
+  beforeCreate(){
+
+  },
+  created(){
+    this.loadTasks();
+  }  
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.expand-enter-active, .expand-leave-active {
+  max-height: 800px;
+  transition: all 1.2s ease;
 }
 
-h1, h2 {
-  font-weight: normal;
+.expand-leave-active {
+  transition: all .7s ease;
+}
+.expand-enter, .expand-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  max-height: 0px;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+section.addTask{
+  overflow: hidden;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+ul.task-list li{
+  display: flex;
 }
 </style>
